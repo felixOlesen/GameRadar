aaa9ef271ba2bfb2da36027a713e9aad6fee7183
Felix Olesen <felixolesen@gmail.com>
2020-11-28 14:53:17 +0000

    Added contact page with testing

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'haml-rails'
 gem 'bootstrap-sass', '~> 3.4.1'
 gem 'modernizr-rails'
 gem 'jquery-rails'
+gem 'rails-controller-testing'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,9 +4,18 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_select'title', 'Game Radar'
-    assert_select'h1', 'Game Radar'
-    assert_select'p', 'Welcome to the ultimate Game Release Radar!'
+    assert_select'div', 'Game Radar'
+    assert_select'div', 'Welcome to the ultimate Game Release Radar!'
     assert_response :success
+  end
+
+  test "should get contact" do
+      get contact_url
+      assert_response :success
+      assert_template layout: 'application'
+      assert_select 'title', 'Game Radar'
+      assert_select 'div', 'Contact Us'
+      assert_select'div', 'Complete the following form to get in touch with us.'
   end
 
 end
