5b806136d8cd03c54e89e3273856edfc314329a7
Felix Olesen <felixolesen@gmail.com>
2020-11-28 16:54:19 +0000

    Added contact form and testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -10,12 +10,27 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get contact" do
-      get contact_url
-      assert_response :success
-      assert_template layout: 'application'
-      assert_select 'title', 'Game Radar'
-      assert_select 'h1', 'Contact Us'
-      assert_select 'p', 'Complete the following form to get in touch with us.'
+    get contact_url
+    assert_response :success
+    assert_template layout: 'application'
+    assert_select 'title', 'Game Radar'
+    assert_select 'h1', 'Contact Us'
+    assert_select 'p', 'Complete the following form to get in touch with us.'
+  end
+
+  test "should post request contact but no email" do
+    post request_contact_url
+
+    assert_response :redirect
+    assert_not_empty flash[:alert]
+    assert_nil flash[:notice]
+  end
+
+  test "should do post request contact" do
+    post request_contact_url, params: {name: "Matther", email: "matthew@me.com", message: "Hello"}
+    assert_response :redirect
+    assert_nil flash[:alert]
+    assert_not_empty flash[:notice]
   end
 
 end
