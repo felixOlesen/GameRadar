446f2b69e6d5b8579157e508e762553c47932a15
Felix Olesen <felixolesen@gmail.com>
2020-11-28 11:39:07 +0000

    Added testing to Home Controller

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,10 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
+    assert_select'title', 'Game Radar'
+    assert_select'h1', 'Game Radar'
+    assert_select'p', 'Welcome to the ultimate Game Release Radar!'
     assert_response :success
   end
 
