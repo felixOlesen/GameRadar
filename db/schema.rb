19a12f853ba9642623e7099077d98cf5230b9726
Felix Olesen <felixolesen@gmail.com>
2020-12-06 11:28:21 +0000

    Migrated forum scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_06_103634) do
+ActiveRecord::Schema.define(version: 2020_12_06_112055) do
+
+  create_table "forums", force: :cascade do |t|
+    t.integer "game_id", null: false
+    t.string "title", null: false
+    t.text "entry", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_forums_on_game_id"
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "name", null: false
