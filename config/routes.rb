d8568040cb20ebae9fbc0fbdb8cd469a68c201e9
Felix Olesen <felixolesen@gmail.com>
2020-12-11 17:53:48 +0000

    Edited navbar routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   resources :forums
   resources :games
+  #All routes for home controller
   root 'home#home'
   get 'contact', to: 'home#contact'
   post 'request_contact', to: 'home#request_contact'
   get 'search', to: 'home#gb_search'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
